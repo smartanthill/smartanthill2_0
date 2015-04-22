31075e62d278c05e80eda8894a1278a25ec663bd
divanchykhin <di@o-log-n.com>
2015-04-22 11:07:47 +0300

    Emulator actual embodiment: SASP code cleanup

--- tests/emulator/sasp_protocol.cpp
+++ tests/emulator/sasp_protocol.cpp
@@ -28,9 +28,7 @@ void SASP_initAtLifeStart( uint8_t* dataBuff )
 {
 	memset( dataBuff + DATA_SASP_NONCE_LW_OFFSET, 0, SASP_NONCE_SIZE );
 	memset( dataBuff + DATA_SASP_NONCE_LS_OFFSET, 0, SASP_NONCE_SIZE );
-//	*( dataBuff + DATA_SASP_NONCE_LS_OFFSET + SASP_NONCE_SIZE - 1 ) = 1;
 	*( dataBuff + DATA_SASP_NONCE_LS_OFFSET ) = 1;
-//	memset( dataBuff + DATA_SASP_LRPS_OFFSET, 0, SASP_TAG_SIZE );
 
 	eeprom_write( DATA_SASP_NONCE_LW_ID, dataBuff + DATA_SASP_NONCE_LW_OFFSET, SASP_NONCE_SIZE );
 	eeprom_write( DATA_SASP_NONCE_LW_ID, dataBuff + DATA_SASP_NONCE_LW_OFFSET, SASP_NONCE_SIZE );
@@ -38,8 +36,6 @@ void SASP_initAtLifeStart( uint8_t* dataBuff )
 
 void SASP_restoreFromBackup( uint8_t* dataBuff )
 {
-//	memset( dataBuff + DATA_SASP_LRPS_OFFSET, 0, SASP_TAG_SIZE );
-
 	uint8_t size;
 
 	size = eeprom_read_size( DATA_SASP_NONCE_LW_ID );
@@ -71,23 +67,8 @@ int8_t SASP_NonceCompare( const uint8_t* nonce1, const uint8_t* nonce2 )
 	}
 	return 0;
 }
-/*
-bool SASP_NonceIsIntendedForSasp(  const uint8_t* nonce )
-{
-	return nonce[SASP_NONCE_SIZE-1] & ((uint8_t)0x80);
-}
 
-void SASP_NonceSetIntendedForSaspFlag(  uint8_t* nonce )
-{
-	nonce[SASP_NONCE_SIZE-1] |= 0x80;
-}
-
-void SASP_NonceClearForSaspFlag(  uint8_t* nonce )
-{
-	nonce[SASP_NONCE_SIZE-1] &= 0x7F;
-}
-*/
-void SASP_EncryptAndAddAuthenticationData( REQUEST_REPLY_HANDLE mem_h/*, uint8_t* _stack, int stackSize*/, const uint8_t* key, const uint8_t* packet_id )
+void SASP_EncryptAndAddAuthenticationData( REQUEST_REPLY_HANDLE mem_h, const uint8_t* key, const uint8_t* packet_id )
 {
 	// msgSize covers the size of message_byte_sequence
 	// (byte with MASTER_SLAVE_BIT) | nonce concatenation is used as a full nonce
@@ -108,59 +89,7 @@ void SASP_EncryptAndAddAuthenticationData( REQUEST_REPLY_HANDLE mem_h/*, uint8_t
 	parser_obj po, po1;
 	zepto_parser_init( &po, mem_h );
 
-	// init stack
-//	uint8_t* stack_start = _stack;
-//	uint8_t* stack = _stack;
 
-	// read first byte
-/*	uint8_t first_byte = zepto_parse_uint8( &po );
-	uint16_t msg_size = zepto_parsing_remaining_bytes( &po );
-
-	zepto_parser_init( &po1, &po );
-	zepto_parse_skip_block( &po1, msg_size );
-	zepto_convert_part_of_request_to_response( mem_h, &po, &po1 );
-
-	uint16_t padding_size = SASP_ENC_BLOCK_SIZE - ( msg_size + 1 ) % SASP_ENC_BLOCK_SIZE;
-
-	if ( padding_size )
-	{
-		first_byte |= 0x80; // TODO: use bit-field processing instead
-		zepto_write_prepend_byte( mem_h, padding_size );
-		zepto_write_prepend_byte( mem_h, first_byte );
-		zepto_write_block( mem_h, block, padding_size - 1 ); // TODO 1: check sizes; TODO 2: it's a good place to add some fancy padding (say, all zeros, or rand generated)
-	}
-	else
-	{
-		zepto_write_prepend_byte( mem_h, first_byte );
-	}
-	zepto_write_prepend_byte( mem_h, first_byte );
-
-	zepto_response_to_request( mem_h );
-	zepto_parser_init( &po, mem_h );*/
-
-	// encrypt and generate authentication data
-/*	while ( zepto_parsing_remaining_bytes( &po ) )
-	{
-		bool read_ok = zepto_parse_read_block( &po, stack, SASP_ENC_BLOCK_SIZE );
-		if ( !read_ok )
-		{
-			assert( 0 == "block size is not multiple of SASP_ENC_BLOCK_SIZE" );
-			return; // TODO: process!!!
-		}
-
-		// do DUMMY authentication/decryption
-		for ( i=0; i<SASP_ENC_BLOCK_SIZE; i++)
-			stack[ i ] ^= 'y'; // dummy encrypt
-		for ( i=0; i<SASP_ENC_BLOCK_SIZE; i++)
-			c ^= stack[ i ]; // toward dummy tag
-
-		// write decrypted block to output
-		zepto_write_block( mem_h, stack, SASP_ENC_BLOCK_SIZE );
-	}
-
-	// form DUMMY tag
-	for ( i=0; i<SASP_TAG_SIZE; i++)
-		stack[i] = c;*/
 	if ( zepto_parsing_remaining_bytes( &po ) )
 	{
 		uint8_t block[16];
@@ -194,17 +123,7 @@ void SASP_EncryptAndAddAuthenticationData( REQUEST_REPLY_HANDLE mem_h/*, uint8_t
 
 	// uppend tag, and init and prepend prefix
 	zepto_write_block( mem_h, tag, SASP_TAG_SIZE );
-
-/*	uint8_t* prefix = nonce; // reuse memory
-	uint8_t* pr_end = prefix;
-	zepto_parser_encode_uint( packet_id, SASP_NONCE_SIZE, &pr_end );
-	zepto_write_prepend_block( mem_h, prefix, pr_end - prefix );*/
 	zepto_parser_encode_and_prepend_uint( mem_h, packet_id, SASP_NONCE_SIZE );
-
-//	zepto_write_prepend_block( mem_h, nonce, SASP_NONCE_SIZE );
-/*	uint8_t* block_end = block;
-	zepto_parser_encode_uint( block, SASP_NONCE_SIZE, &block_end );
-	zepto_write_prepend_block( mem_h, block, block_end - block );*/
 }
 
 bool SASP_IntraPacketAuthenticateAndDecrypt( const uint8_t* key, REQUEST_REPLY_HANDLE mem_h, uint8_t* pid ) 
@@ -213,34 +132,21 @@ bool SASP_IntraPacketAuthenticateAndDecrypt( const uint8_t* key, REQUEST_REPLY_H
 	// Structure of output buffer: first_byte | byte_sequence
 	// pid (Packet ID) is formed from prefix data
 	
-	
-	// DUMMY values used only until actual authentication/decryption is implemented
-/*	uint8_t nonce[ SASP_NONCE_SIZE ];
-	uint8_t header[ SASP_HEADER_SIZE ];*/
 	uint8_t block[ SASP_ENC_BLOCK_SIZE ];
 	uint8_t tag_calculted[16];
 	uint8_t ctr[16];
 	uint8_t msg_cbc_val[16];
-//	uint8_t* header_prim = ctr; // we will reuse it when ctr is no longer needed
 
 	// init parser object
 	parser_obj po;
 	zepto_parser_init( &po, mem_h );
-
-	// init stack
-/*	uint8_t* stack_start = _stack;
-	uint8_t* stack = _stack;*/
 
 	// read and form PID
 	uint8_t nonce[ 7 ];
 	zepto_parser_decode_uint( &po, nonce, 6 );
 	memcpy( pid, nonce, 6 );
 	nonce[6] = 1 - MASTER_SLAVE_BIT;
-/*	pid[ SASP_NONCE_SIZE - 1] &= 0x7F; // remove SASP bit; TODO: potentially, we need to use bit-fiels processing instead
-	uint8_t nonce[ SASP_NONCE_SIZE ];
-	memcpy( nonce, pid, SASP_HEADER_SIZE );
-	nonce[ SASP_NONCE_SIZE - 1] &= ( 1 - MASTER_SLAVE_BIT ) << 7; // add sending sidde bit; TODO: potentially, we need to use bit-fiels processing instead
-*/
+
 	// read blocks one by one, authenticate, decrypt, write
 	bool read_ok;
 	if ( zepto_parsing_remaining_bytes( &po ) > SASP_TAG_SIZE )
@@ -251,12 +157,6 @@ bool SASP_IntraPacketAuthenticateAndDecrypt( const uint8_t* key, REQUEST_REPLY_H
 			read_ok = zepto_parse_read_block( &po, block, SASP_ENC_BLOCK_SIZE );
 			if ( !read_ok )
 				return false; // if any bytes, then the whole block
-
-			// do DUMMY authentication/decryption
-	/*		for ( i=0; i<SASP_ENC_BLOCK_SIZE; i++)
-				stack[ i ] ^= 'y'; // dummy decrypt
-			for ( i=0; i<SASP_ENC_BLOCK_SIZE; i++)
-				c ^= stack[ i ]; // toward dummy tag*/
 
 			eax_128_process_nonterminating_block_decr( key, ctr, block, block, msg_cbc_val );
 			// write decrypted block to output
@@ -283,42 +183,6 @@ bool SASP_IntraPacketAuthenticateAndDecrypt( const uint8_t* key, REQUEST_REPLY_H
 	for ( uint8_t i=0; i<SASP_TAG_SIZE; i++ )
 		if ( tag_calculted[i] != block[i] ) return false;
 
-	// decrease stack pointer to point again to tag
-
-	// read tag; note that we do not need it until the end of authentication, so, in general, it could be read later (if proper parser calls are available)
-/*	read_ok = zepto_parse_read_block( &po, stack, SASP_ENC_BLOCK_SIZE );
-	zepto_parse_read_block( &po, stack, SASP_TAG_SIZE );
-	if ( !read_ok )
-		return false; // if any bytes, then the whole block
-//	stack += SASP_TAG_SIZE;
-//	stack -= SASP_TAG_SIZE;
-
-	// do DUMMY authentication
-	bool tag_ok = true;
-	for ( i=0; i<SASP_TAG_SIZE; i++)
-		tag_ok = tag_ok && stack[i] == c;
-
-	if ( !tag_ok )
-		return false;*/
-
-/*	zepto_response_to_request( mem_h );
-	zepto_parser_init( &po, mem_h );
-
-	uint8_t first_byte = zepto_parse_uint8( &po );
-
-	uint16_t padding_size = 0;
-	if ( first_byte & 0x80 ) // TODO: use bit-fiels processing instead
-		padding_size = zepto_parse_uint8( &po ) - 1; // TODO: NOTE!!!! in the new implementation this MAY be revised
-	uint16_t body_size = zepto_parsing_remaining_bytes( &po ) - padding_size;
-	uint16_t body_size = zepto_parsing_remaining_bytes( &po );
-
-	parser_obj po1;
-	zepto_parser_init( &po1, &po );
-	zepto_parse_skip_block( &po1, body_size );
-	zepto_convert_part_of_request_to_response( mem_h, &po, &po1 );
-	first_byte &= 0x7; // TODO: use bit-fiels processing instead
-	zepto_write_prepend_byte( mem_h, first_byte );*/
-
 	return true;
 }
 
@@ -342,7 +206,7 @@ bool SASP_is_for_sasp( REQUEST_REPLY_HANDLE mem_h )
 	return for_sasp;
 }
 
-void DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( MEMORY_HANDLE mem_h/**/, const uint8_t* key, const uint8_t* nonce )
+void DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( MEMORY_HANDLE mem_h, const uint8_t* key, const uint8_t* nonce )
 {
 	uint8_t header[6];
 
@@ -352,20 +216,14 @@ void DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( MEMORY_HANDLE mem_h/**/,
 
 	PRINTF( "handler_sasp_send(): nonce used: %02x %02x %02x %02x %02x %02x\n", nonce[0], nonce[1], nonce[2], nonce[3], nonce[4], nonce[5] );
 	PRINTF( "handler_sasp_send(): size: %d\n", inisz );
-/*	if ( inisz == 0 )
-	{
-		inisz = 0;
-	}*/
 
 	uint16_t encr_sz, decr_sz;
 	uint8_t inimsg[1024]; zepto_parse_read_block( &po, inimsg, inisz );
 	uint8_t checkedMsg[1024];
-//	uint8_t dbg_stack[1024];
 	uint8_t dbg_nonce[6];
 
-
 	// do required job
-	SASP_EncryptAndAddAuthenticationData( mem_h/*, stack, stackSize*/, key, nonce );
+	SASP_EncryptAndAddAuthenticationData( mem_h, key, nonce );
 
 	// CHECK RESULTS
 
@@ -375,12 +233,8 @@ void DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( MEMORY_HANDLE mem_h/**/,
 
 
 	// TODO: if this debug code remains in use, lines below must be replaced by getting a new handle
-	uint8_t /*x_buff1_x[1024],*/ x_buff1_x2[1024];
-/*	REQUEST_REPLY_HANDLE mem_h2 = 110;
-	uint8_t* x_buff1 = x_buff1_x + 512;
-	memory_objects[ mem_h2 ].ptr = x_buff1;
-	memory_objects[ mem_h2 ].rq_size = 0;
-	memory_objects[ mem_h2 ].rsp_size = 0;*/
+	uint8_t x_buff1_x2[1024];
+
 	// copy output to input of a new handle and restore
 	zepto_response_to_request( mem_h );
 	encr_sz = zepto_parsing_remaining_bytes( &po1 );
@@ -403,19 +257,16 @@ void DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( MEMORY_HANDLE mem_h/**/,
 	assert( ipaad );
 	assert( decr_sz == inisz );
 	assert( memcmp( nonce, dbg_nonce, 6 ) == 0 );
-//	checkedMsg[0] &= 0x7F; // get rid of SASP bit
 	for ( int k=0; k<decr_sz; k++ )
 		assert( inimsg[k] == checkedMsg[k] );
 }
 
 uint8_t handler_sasp_send( const uint8_t* key, const uint8_t* packet_id, MEMORY_HANDLE mem_h, uint8_t* data )
 {
-//	, header[6];
-
 	assert( SASP_NonceCompare( packet_id, data + DATA_SASP_NONCE_LS_OFFSET ) >= 0 );
 
-//	SASP_EncryptAndAddAuthenticationData( mem_h/*, stack, stackSize*/, packet_id );
-	DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( mem_h/*, stack, stackSize*/, key, packet_id );
+//	SASP_EncryptAndAddAuthenticationData( mem_h, packet_id );
+	DEBUG_SASP_EncryptAndAddAuthenticationDataChecked( mem_h, key, packet_id );
 
 	INCREMENT_COUNTER( 10, "handler_sasp_send()" );
 	return SASP_RET_TO_LOWER_REGULAR;
@@ -436,21 +287,7 @@ uint8_t handler_sasp_receive( const uint8_t* key, uint8_t* pid, MEMORY_HANDLE me
 		return SASP_RET_IGNORE;
 	}
 
-//	zepto_parse_read_block( &po, stack, SASP_NONCE_SIZE );
-/*	zepto_parser_decode_uint( &po, header, SASP_HEADER_SIZE );
-	bool for_sasp = SASP_NonceIsIntendedForSasp( header );
-	uint16_t debug_rem_b = zepto_parsing_remaining_bytes( &po );
-	if ( for_sasp )
-	{
-		PRINTF( "handler_sasp_receive(): for sasp; nonce: %02x %02x %02x %02x %02x %02x\n", header[0], header[1], header[2], header[3], header[4], header[5] );
-		PRINTF( "handler_sasp_receive(): packet size: %d\n", zepto_parsing_remaining_bytes( &po ) );
-	}*/
-
 	bool ipaad = SASP_IntraPacketAuthenticateAndDecrypt( key, mem_h, pid );
-/*	if ( ipaad && for_sasp )
-	{
-		assert( debug_rem_b == 2 * SASP_ENC_BLOCK_SIZE );
-	}*/
 
 	PRINTF( "handler_sasp_receive(): PID: %02x %02x %02x %02x %02x %02x\n", pid[0], pid[1], pid[2], pid[3], pid[4], pid[5] );
 	if ( !ipaad )
@@ -467,25 +304,12 @@ uint8_t handler_sasp_receive( const uint8_t* key, uint8_t* pid, MEMORY_HANDLE me
 		zepto_response_to_request( mem_h ); // since now it's we who are the recipient of this packet
 		INCREMENT_COUNTER( 13, "handler_sasp_receive(), intended for SASP" );
 		uint8_t* nls = data+DATA_SASP_NONCE_LS_OFFSET;
-		// now we need to extract a proposed value of nonce from the output
-		// we do some strange manipulations: make output input, parse proper block, make input output
-		// TODO: think whether it is possible to do in more sane way... somehow as it was before
-		// uint8_t* new_nls = buffOut+2;
+
 		parser_obj po1;
 		zepto_parser_init( &po1, mem_h );
-//		zepto_response_to_request( mem_h );
 		zepto_parse_skip_block( &po1, 2 );
-//		zepto_parse_read_block( &po1, stack + SASP_NONCE_SIZE, SASP_NONCE_SIZE );
 		uint8_t new_nls[SASP_HEADER_SIZE];
 		zepto_parser_decode_uint( &po1, new_nls, SASP_HEADER_SIZE );
-/*		zepto_parser_init( &po1, mem_h );
-		parser_obj po2;
-		zepto_parser_init( &po2, mem_h );
-		uint16_t packet_size = zepto_parsing_remaining_bytes( &po2 );
-		zepto_parse_skip_block( &po2, packet_size );
-		zepto_convert_part_of_request_to_response( mem_h, &po1, &po2 ); // finally, put where taken...*/
-
-//		uint8_t* new_nls = stack + SASP_NONCE_SIZE;
 
 		PRINTF( "handler_sasp_receive(): packet for SASP received...\n" );
 		PRINTF( "   current  NLS: %02x %02x %02x %02x %02x %02x\n", nls[0], nls[1], nls[2], nls[3], nls[4], nls[5] );
@@ -495,7 +319,6 @@ uint8_t handler_sasp_receive( const uint8_t* key, uint8_t* pid, MEMORY_HANDLE me
 		if ( nonceCmp < 0 )
 		{
 			INCREMENT_COUNTER( 14, "handler_sasp_receive(), nonce las updated" );
-//			SASP_NonceClearForSaspFlag( new_nls );
 			memcpy( nls, new_nls, SASP_NONCE_SIZE );
 			SASP_NonceLS_increment( nls );
 			// TODO: shuold we do anything else?
@@ -506,8 +329,6 @@ uint8_t handler_sasp_receive( const uint8_t* key, uint8_t* pid, MEMORY_HANDLE me
 	}
 
 	// 3. Compare nonces...
-//	uint8_t ne[ SASP_HEADER_SIZE ];
-//	uint8_t* ne_end = ne;
 	uint8_t* nlw = data+DATA_SASP_NONCE_LW_OFFSET;
 	int8_t nonceCmp = SASP_NonceCompare( pid, nlw );
 	if ( nonceCmp <= 0 ) // error message must be prepared
@@ -519,35 +340,21 @@ uint8_t handler_sasp_receive( const uint8_t* key, uint8_t* pid, MEMORY_HANDLE me
 
 		zepto_response_to_request( mem_h ); // we have to create a new response (with error old nonce)
 		// we are to create a brand new output
-		// TODO: think about faster and more straightforward approaches
-/*		uint8_t* ins_ptr = stack;
-		*(ins_ptr++) = 0;
-		*(ins_ptr++) = 0; //  Byte
-		memcpy( ins_ptr, nlw, SASP_NONCE_SIZE );
-		ins_ptr += SASP_NONCE_SIZE;
-		*sizeInOut = SASP_NONCE_SIZE+2;*/
 		zepto_write_uint8( mem_h, 0x80 ); // First Byte; TODO: use bit field processing instead (should we?)
 		zepto_write_uint8( mem_h, 0 ); // Reserved Byte
-//		zepto_write_block( mem_h, nlw, SASP_NONCE_SIZE );
-/*		zepto_parser_encode_uint( nlw, SASP_NONCE_SIZE, &ne_end );
-		zepto_write_block( mem_h, ne, ne_end - ne );*/
 		zepto_parser_encode_and_append_uint( mem_h, nlw, SASP_NONCE_SIZE );
 		zepto_response_to_request( mem_h );
 
 		uint8_t ne[ SASP_HEADER_SIZE ];
 		memcpy( ne, pid, SASP_NONCE_SIZE );
-//		SASP_NonceSetIntendedForSaspFlag( ne );
-		SASP_EncryptAndAddAuthenticationData( mem_h/*, stack + SASP_NONCE_SIZE, stackSize - SASP_NONCE_SIZE*/,key, ne );
+		SASP_EncryptAndAddAuthenticationData( mem_h,key, ne );
 		PRINTF( "handler_sasp_receive(): ------------------- ERROR OLD NONCE WILL BE SENT ----------------------\n" );
 		assert( ugly_hook_get_response_size( mem_h ) <= 39 );
 		return SASP_RET_TO_LOWER_ERROR;
 	}
 
 	// 4. Finally, we have a brand new packet
-//	memcpy( data+DATA_SASP_NONCE_LW_OFFSET, ne, SASP_NONCE_SIZE ); // update Nonce Low Watermark
 	memcpy( data+DATA_SASP_NONCE_LW_OFFSET, pid, SASP_NONCE_SIZE ); // update Nonce Low Watermark
-//	memcpy( data+DATA_SASP_LRPS_OFFSET, buffIn+SASP_HEADER_SIZE, SASP_TAG_SIZE ); // save packet signature
-//	pid[ SASP_NONCE_SIZE - 1 ] &= (uint8_t)(0x7F);
 	INCREMENT_COUNTER( 16, "handler_sasp_receive(), passed up" );
 	return SASP_RET_TO_HIGHER_NEW;
 }
@@ -556,6 +363,5 @@ uint8_t handler_sasp_get_packet_id( uint8_t* buffOut, int buffOutSize, uint8_t* 
 {
 	SASP_NonceLS_increment( data + DATA_SASP_NONCE_LS_OFFSET );
 	memcpy( buffOut, data + DATA_SASP_NONCE_LS_OFFSET, SASP_NONCE_SIZE );
-//	buffOut[ SASP_NONCE_SIZE - 1 ] &= (uint8_t)(0x7F);
 	return SASP_RET_NONCE;
 }
