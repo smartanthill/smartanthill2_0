5489b9f5cc0de85529aa1fa110b0a52d914677f8
Dmytro Kyrychuk <dmytro.kyrychuck@gmail.com>
2015-05-15 13:17:57 +0300

    More informative exceptions and logging messages for `litemq.service`

--- smartanthill/litemq/service.py
+++ smartanthill/litemq/service.py
@@ -26,11 +26,15 @@ class LiteMQService(SAMultiService):
         self._exchanges = {}
 
     def stopService(self):
-        assert self._exchanges == {}
+        if self._exchanges:
+            raise Exception("Non-empty exchanges dict: %s"
+                            % (self._exchanges,))
         SAMultiService.stopService(self)
 
     def declare_exchange(self, name, type_="direct"):
         if name in self._exchanges:
+            self.log.warn("Attempt to declare already declared exchange '%s' "
+                          "with type '%s'" % (name, type_))
             return
         self._exchanges[name] = ExchangeFactory().newExchange(name, type_)
         self.log.info("Declared new exchange '%s' with type '%s'" % (
@@ -43,7 +47,7 @@ class LiteMQService(SAMultiService):
         self.log.info("Undeclared exchange '%s'" % name)
 
     def produce(self, exchange, routing_key, message, properties=None):
-        assert exchange in self._exchanges
+        self._ensure_exchange_declared(exchange)
         self.log.debug(
             "Produce new message '%s' with routing_key '%s' to exchange '%s'" %
             (hexlify(message) if properties and "binary" in properties and
@@ -52,17 +56,21 @@ class LiteMQService(SAMultiService):
                                                  properties)
 
     def consume(self, exchange, queue, routing_key, callback, ack=False):
-        assert exchange in self._exchanges
+        self._ensure_exchange_declared(exchange)
         self._exchanges[exchange].bind_queue(queue, routing_key, callback, ack)
         self.log.info("Registered consumer with exchange=%s, queue=%s, "
                       "routing_key=%s, ack=%s" % (exchange, queue, routing_key,
                                                   ack))
 
     def unconsume(self, exchange, queue):
-        assert exchange in self._exchanges
+        self._ensure_exchange_declared(exchange)
         self._exchanges[exchange].unbind_queue(queue)
         self.log.info("Unregistered consumer with exchange=%s "
                       "and queue=%s" % (exchange, queue))
+
+    def _ensure_exchange_declared(self, exchange):
+        if exchange not in self._exchanges:
+            raise Exception("Exchange is not declared: %s" % exchange)
 
 
 def makeService(name, options):
