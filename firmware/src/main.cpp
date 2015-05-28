8d85248e164c14e9ecbce7bd7f3712a7f004d73c
Ivan Kravets <me@ikravets.com>
2015-05-28 16:23:59 +0300

    Preparation for SmartAnthill Embedded 2.0

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -15,27 +15,12 @@
   51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  */
 
-#include "main.h"
-
-/* int freeRam () {*/
-/*   extern int __heap_start, *__brkval;*/
-/*   int v;*/
-/*   return (int) &v - (__brkval == 0 ? (int) &__heap_start : (int) __brkval);*/
-/* }*/
-
-
 void setup()
 {
-    routerInit();
-    /* Serial.println(freeRam());*/
+
 }
 
 void loop()
 {
-    routerLoop();
 
-    if (routerHasInPacket())
-        osm.makeTransition(osm.findStateByCDC(routerGetInPacket()->cdc));
-
-    osm.updateState();
 }
