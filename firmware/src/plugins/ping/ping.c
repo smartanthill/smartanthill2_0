6590d472130fc7e03f63697c9150ff129a832d3b
Ivan Kravets <me@ikravets.com>
2015-06-07 23:06:29 +0300

    Add "ping" plugin

---
+++ firmware/src/plugins/ping/ping.c
@@ -0,0 +1,30 @@
+/*******************************************************************************
+Copyright (C) 2015 OLogN Technologies AG
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+#include "ping.h"
+
+uint8_t ping_plugin_handler_init( const void* plugin_config, void* plugin_state )
+{
+	return 0;
+}
+
+uint8_t ping_plugin_handler( const void* plugin_config, void* plugin_state, parser_obj* command, MEMORY_HANDLE reply/*, WaitingFor* waiting_for*/, uint8_t first_byte )
+{
+	zepto_response_to_request( reply );
+	zepto_write_uint8( reply, 1 ); // answer with "1", we are on-line!
+	return 0;
+}
---
+++ firmware/src/plugins/ping/ping.h
@@ -0,0 +1,39 @@
+/*******************************************************************************
+Copyright (C) 2015 OLogN Technologies AG
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+
+#if !defined __SA_PING_PLUGIN_H__
+#define __SA_PING_PLUGIN_H__
+
+#include "../../common/sa-common.h"
+#include "../../common/sa-data-types.h"
+
+typedef struct _PingPluginConfig
+{
+
+} PingPluginConfig;
+
+typedef struct _PingPluginState
+{
+
+} PingPluginState;
+
+uint8_t ping_plugin_handler_init( const void* plugin_config, void* plugin_state );
+uint8_t ping_plugin_handler( const void* plugin_config, void* plugin_state, parser_obj* command, MEMORY_HANDLE reply/*, WaitingFor* waiting_for*/, uint8_t first_byte );
+
+
+#endif // __SA_PING_PLUGIN_H__
