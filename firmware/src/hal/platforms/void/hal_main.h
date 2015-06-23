4c53d65a4cb55a9c000f5bbe9caf5407a00b6620
divanchykhin <di@o-log-n.com>
2015-06-23 14:45:46 +0300

    adding "void" platform (for testing purposes)

---
+++ firmware/src/hal/platforms/void/hal_main.h
@@ -0,0 +1,36 @@
+/*******************************************************************************
+Copyright (C) 2015 OLogN Technologies AG
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+#if !defined __HAL_PLATFORM_VOID_MAIN_H__
+#define __HAL_PLATFORM_VOID_MAIN_H__
+
+#include "stdint.h"
+#include "hal-time-conversions.h"
+
+#if defined __AVR
+#define ZEPTO_PROGMEM_IN_USE
+#endif
+
+#ifdef ZEPTO_PROGMEM_IN_USE
+#include <avr/pgmspace.h>
+#define ZEPTO_PROGMEM      __attribute__ ((progmem))
+#define ZEPTO_PROG_CONSTANT_LOCATION ZEPTO_PROGMEM
+#define ZEPTO_PROG_CONSTANT_READ_BYTE(x) pgm_read_byte(x)
+#define ZEPTO_MEMCPY_FROM_PROGMEM memcpy_PF
+#endif // ZEPTO_PROGMEM_IN_USE
+
+#endif // __HAL_PLATFORM_VOID_MAIN_H__
---
+++ firmware/src/hal/platforms/void/sa_hal_time_provider.c
@@ -0,0 +1,37 @@
+/*******************************************************************************
+Copyright (C) 2015 OLogN Technologies AG
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License version 2 as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*******************************************************************************/
+
+#include "../../sa-hal-time-provider.h"
+
+void sa_get_time(sa_time_val* t)
+{
+}
+
+unsigned short getTime()
+{
+    return 0;
+}
+
+void mcu_sleep( uint16_t sec, uint8_t transmitter_state_on_exit )
+{
+}
+
+void just_sleep( sa_time_val* timeval )
+{
+
+}
+
